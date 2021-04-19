0ee15451a1aaf961d781f0b6b1ecd7955b54dbac
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2021-04-19 11:21:28 +0100

    added export script

---
+++ webpack.mix.js
@@ -0,0 +1,102 @@
+/**
+ * Laravel Mix configuration file.
+ *
+ * Laravel Mix is a layer built on top of WordPress that simplifies much of the
+ * complexity of building out a Webpack configuration file. Use this file to
+ * configure how your assets are handled in the build process.
+ *
+ * This configuration is a copy of the configuration from https://github.com/justintadlock/mythic/
+ * modified to fit Pressmodo's plugins purposes.
+ */
+
+// Import required packages.
+const mix = require( 'laravel-mix' );
+
+/*
+ * -----------------------------------------------------------------------------
+ * Plugin Export Process
+ * -----------------------------------------------------------------------------
+ * Configure the export process in `webpack.mix.export.js`. This bit of code
+ * should remain at the top of the file here so that it bails early when the
+ * `export` command is run.
+ * -----------------------------------------------------------------------------
+ */
+
+if ( process.env.export ) {
+	const exportPlugin = require( './webpack.mix.export.js' );
+	return;
+}
+
+/*
+ * -----------------------------------------------------------------------------
+ * Build Process
+ * -----------------------------------------------------------------------------
+ * The section below handles processing, compiling, transpiling, and combining
+ * all of the plugin's assets into their final location. This is the meat of the
+ * build process.
+ * -----------------------------------------------------------------------------
+ */
+
+/*
+ * Sets the development path to assets. By default, this is the `/resources`
+ * folder in the plugin.
+ */
+const devPath  = 'resources';
+
+/*
+ * Sets the path to the generated assets. By default, this is the `/dist` folder
+ * in the theme. If doing something custom, make sure to change this everywhere.
+ */
+mix.setPublicPath( 'dist' );
+
+/*
+ * Set Laravel Mix options.
+ *
+ * @link https://laravel.com/docs/5.6/mix#postcss
+ * @link https://laravel.com/docs/5.6/mix#url-processing
+ */
+mix.options( {
+	postCss        : [ require( 'postcss-preset-env' )() ],
+	processCssUrls : false
+} );
+
+/*
+ * Builds sources maps for assets.
+ *
+ * @link https://laravel.com/docs/5.6/mix#css-source-maps
+ */
+mix.sourceMaps();
+
+/*
+ * Versioning and cache busting. Append a unique hash for production assets. If
+ * you only want versioned assets in production, do a conditional check for
+ * `mix.inProduction()`.
+ *
+ * @link https://laravel.com/docs/5.6/mix#versioning-and-cache-busting
+ */
+mix.version();
+
+/*
+ * Compile JavaScript.
+ *
+ * @link https://laravel.com/docs/5.6/mix#working-with-scripts
+ */
+
+/*
+ * Compile CSS. Mix supports Sass, Less, Stylus, and plain CSS, and has functions
+ * for each of them.
+ *
+ * @link https://laravel.com/docs/5.6/mix#working-with-stylesheets
+ * @link https://laravel.com/docs/5.6/mix#sass
+ * @link https://github.com/sass/node-sass#options
+ */
+
+// Sass configuration.
+var sassConfig = {
+	outputStyle : 'expanded',
+	indentType  : 'tab',
+	indentWidth : 1
+};
+
+// Compile SASS/CSS.
+//mix.sass( `${devPath}/scss/style.scss`, 'css', sassConfig );
